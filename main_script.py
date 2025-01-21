bbeb6ad5ed0c7edca367c51e04a099ee64884711
Oscar Ivan <oscar.moreno4275@alumnos.udg.mx>
2025-01-20 20:16:58 -0600

    -Added error messages

--- main_script.py
+++ main_script.py
@@ -7,11 +7,38 @@ import ui_interface as ui
 
 
 def retrieve(title_, language_, code_):
+    flag = False
+    error_message_var_title = ""
+    error_message_var_code = ""
     print("working")
     title = title_.get()
     language = language_.get()
     code = code_.get("1.0", "end-1c")
-    print(f"{title}\n{language}\n{code}")
+    ##displaying error messages
+    ## leetcode title must be a number:
+    if not title.isdigit():
+        error_message_var_title = "Must be a integer"
+        flag = True
+    elif title == "":
+        error_message_var_title = "Cannot be empty, Must be a number"
+        flag = True
+    ## check if code is different tha null
+    if code.strip() == "":
+        error_message_var_code = "Where is the code man?"
+        flag = True
+        
+    if flag:
+        ui.error_message_title.config(text=error_message_var_title)
+        ui.error_message_code.config(text=error_message_var_code)
+        return
+    ##print(f"{title}\n{language}\n{code}")
+    return title, language, code
+
+## creating files
+def create_file(filename, content):
+    return
+
+
 
 
 def main():
--- ui_interface.py
+++ ui_interface.py
@@ -3,14 +3,8 @@ from tkinter import *
 from tkinter import ttk
 
 
-def send_data():
-    print("working")
-    code_retrieved = code_inserted.get("1.0", END)
-    leetCode_title_retrieved = LeetCode_Number_field.get()
-    language_used_retrieved = language_used.get()
-    print(leetCode_title_retrieved)
-    print(language_used_retrieved)
-    print(code_retrieved)
+def validate_integer(P):
+    return P.isdigit() or P == ""
 
 
 def create_ui(on_clicky):
@@ -29,24 +23,38 @@ def create_ui(on_clicky):
 
     LeetCode_Number = Label(main_window, text="LeetCode #")
     LeetCode_Number.grid(column=0, row=1)  ## this is the position
-
-    LeetCode_Number_field = Entry(main_window)
+    ## to validate number
+    validate_input = (main_window.register(validate_integer), '%P')
+    LeetCode_Number_field = Entry(main_window, validate='key', validatecommand=validate_input)
     LeetCode_Number_field.grid(column=1, row=1)  ## position
+    
+    ## Error message for the number title
+    global error_message_title
+    error_message_title = Label(main_window, text="", fg="red")
+    error_message_title.grid(column=1, row=2, columnspan=2)
+    
 
     ## drop down for languages used
     language_used = ttk.Combobox(main_window, width=5)
     ## adding the values
-    language_used["values"] = ("C", "C++", "Swift", "Python")
-    language_used.grid(column=0, row=2, columnspan=2)
+    language_used["values"] = ("C", "C++","Java","Swift", "Python")
+    language_used.grid(column=0, row=3, columnspan=2)
     ## setting the default
     language_used.current(0)
 
+    
     ## Giant text entry box for the code
+    
+    ## Error message
+    global error_message_code
+    error_message_code = Label(main_window, text="" , fg="red")
+    error_message_code.grid(column=0, row=4)
+    
     code_inserted_label = Label(main_window, text="Code:")
-    code_inserted_label.grid(column=0, row=3, columnspan=2)
+    code_inserted_label.grid(column=0, row=5, columnspan=2)
 
     code_inserted = Text(main_window, width=30)
-    code_inserted.grid(column=0, row=4, columnspan=2, rowspan=10, sticky="nsew")
+    code_inserted.grid(column=0, row=6, columnspan=2, rowspan=8, sticky="nsew")
 
     ## Button to send the data
     button = Button(
@@ -54,6 +62,6 @@ def create_ui(on_clicky):
         text="Send",
         command=lambda: on_clicky(LeetCode_Number_field, language_used, code_inserted),
     )
-    button.grid(row=16, column=0, columnspan=2, sticky="nsew", padx=20, pady=10)
+    button.grid(row=15, column=0, columnspan=2, sticky="nsew", padx=20, pady=10)
 
     main_window.mainloop()
